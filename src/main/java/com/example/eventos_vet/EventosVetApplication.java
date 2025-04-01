1295e8a2fc980153796668fdf921f4d8722c6dc8
Felipe <fel.guerrerom@duocuc.cl>
2025-03-31 23:01:32 -0300

    first commit

---
+++ src/main/java/com/example/eventos_vet/EventosVetApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventos_vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosVetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventos_vet/EventosVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventos_vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
